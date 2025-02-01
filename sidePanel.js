6bf5451dee9ce416657dc5cf604acc6d1f38019d
Kuma1411 <rounit6111@gmail.com>
2025-02-02 00:49:53 +0530

    Create glow button click event handler in sidepanel

--- sidePanel.js
+++ sidePanel.js
@@ -34,6 +34,13 @@ function handleNavigate(){
   }
 }
 
+chrome.runtime.onMessage.addListener((obj, sender, response) => {
+  const { type, body } = obj;
+
+  if (type === "elementClicked") {
+    console.log("Response from contentScript.js:", body);
+  }
+});
 
 async function handleSubmit() {
   const textField = document.getElementById('text-input');
@@ -43,7 +50,7 @@ async function handleSubmit() {
     const newUser = document.createElement('div');
     newUser.innerHTML = userChatHtml;
     const newMessage = newUser.querySelector('#user-message');
-    newMessage.textContent = message
+    newMessage.textContent = message;
     const chatbox = document.getElementById("chatArea");
     chatbox.appendChild(newUser);
     textField.value = '';
@@ -51,59 +58,59 @@ async function handleSubmit() {
       behavior: 'smooth',
       block: 'end'
     });
-   if(isNavigate){
+
+    if (isNavigate) {
+      chrome.tabs.sendMessage(activeTab.id, {
+        type: "extractDOM",
+        body: "dom"
+      }, async (response) => {
+        console.log(JSON.stringify({ prompt: message, context: response }));
+
+        // Mock response for testing
+        const data = { "instructions": "Click on add widgets", "name": "Add Widgets" };
+        const parsedPrediction = data; // JSON.parse(data);
+        console.log(parsedPrediction);
+
+        if (parsedPrediction) {
+          chrome.tabs.sendMessage(activeTab.id, {
+            type: "navigate",
+            body: parsedPrediction.name
+          });
+        }
+
+        const cloudPilot = document.getElementById("cloudPilot");
+        const newCloudPilot = cloudPilot.cloneNode(true);
+        const newMessage = newCloudPilot.querySelector('#cloudPilot-message');
+        newMessage.textContent = parsedPrediction.instructions;
+        const chatbox = document.getElementById("chatArea");
+        chatbox.appendChild(newCloudPilot);
+        newCloudPilot.scrollIntoView({
+          behavior: 'smooth',
+          block: 'end'
+        });
+
+        const submitButton = document.getElementById('submit-btn');
+        submitButton.removeEventListener('click', handleSubmit);
+        submitButton.style.backgroundColor = "grey";
+      });
+    }
+  }else{
     chrome.tabs.sendMessage(activeTab.id, {
       type: "extractDOM",
-      body:"dom"
+      body: "dom"
     }, async (response) => {
       console.log(JSON.stringify({prompt:message,context:response}))
-      // const resp = await fetch('http://127.0.0.1:8000/navigate',{
+      // const resp = await fetch('http://127.0.0.1:8000/instruct',{
       //   method:'POST',
       //   headers: {
       //     'Content-Type': 'application/json'
       //   },
       //   body:JSON.stringify({prompt:message,context:response})
       // })
-  
+
       // const data = await resp.json();
-      // const parsedPrediction = JSON.parse(data.prediction);
-      // console.log(parsedPrediction)
-      // if (parsedPrediction) {
-      //   chrome.tabs.sendMessage(activeTab.id, {
-      //     type: "navigate",
-      //     body: parsedPrediction.name
-      //   });
-      // }
-      // const cloudPilot = document.getElementById("cloudPilot");
-      // const newCloudPilot = cloudPilot.cloneNode(true);
-      // const newMessage = newCloudPilot.querySelector('#cloudPilot-message');
-      // newMessage.textContent = parsedPrediction.instructions;
-      // const chatbox = document.getElementById("chatArea");
-      // chatbox.appendChild(newCloudPilot);
-      // newCloudPilot.scrollIntoView({
-      //   behavior: 'smooth',
-      //   block: 'end'
-      // });
-      const submitButton = document.getElementById('submit-btn');
-      submitButton.removeEventListener('click',handleSubmit)
-      submitButton.style.backgroundColor = "grey"
-      
-    });
-  }else{
-    chrome.tabs.sendMessage(activeTab.id, {
-      type: "extractDOM",
-      body:"dom"
-    }, async (response) => {
-      console.log(JSON.stringify({prompt:message,context:response}))
-      const resp = await fetch('http://127.0.0.1:8000/instruct',{
-        method:'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body:JSON.stringify({prompt:message,context:response})
-      })
-      const data = await resp.json();
-      const parsedPrediction = JSON.parse(data.prediction);
+      const data = {"instructions": "Click on a widgets"}
+      const parsedPrediction = JSON.parse(data);
       console.log(parsedPrediction);
       const cloudPilot = document.getElementById("cloudPilot");
       const newCloudPilot = cloudPilot.cloneNode(true);
@@ -116,10 +123,6 @@ async function handleSubmit() {
         block: 'end'
       });
     });
-  }
-
-  } else {
-    console.log('Textarea is empty!');
   }
 }
 
